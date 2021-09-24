fd553278e5db7fbe340a92dd88b954c8046fb2b1
NagendraSaiN <nnagendra15@gmail.com>
2021-09-24 16:57:51 +0530

    day2 updated

---
+++ Day2-File/MobileLibrary/Mobile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileLibrary
+{
+    public class Mobile
+    {
+        public void MobileTypes()
+        {
+            Console.WriteLine("Mobile Types");
+            Console.WriteLine("Android");
+            Console.WriteLine("IOS");
+        }
+        public void MobileModels(string MobileTypes)
+        {
+            if(MobileTypes == "IOS")
+            {
+                Console.WriteLine("Apple");
+            }
+        }
+    }
+}
---
+++ Day2-File/MobileLibrary/MobileServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileLibrary
+{
+    public class MobileServices
+    {
+        public void ServiceTypes()
+        {
+            Console.WriteLine("First Time Services");
+        }
+    }
+}
