affefd5a415b95172d1e3d07364c0623d9138f63
NagendraSaiN <nnagendra15@gmail.com>
2021-10-11 13:32:26 +0530

    Department Webform Updated

---
+++ Assignment4-08-10-21/Assignment3/Department.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment4
+{
+    public partial class Department : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                DataBase dataBase = new DataBase();
+                DataTable dt = dataBase.GetDepartment();
+                ddldept_num.Items.Add("---------Select---------");
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    ddldept_num.Items.Add(new ListItem(dt.Rows[i][0].ToString() + " - " + dt.Rows[i][1].ToString(), dt.Rows[i][0].ToString()));
+                }
+                DataTable dtCustomerResult = dataBase.GetDepartment();
+                gvDeptDetails.DataSource = dtCustomerResult;
+                gvDeptDetails.DataBind();
+
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            string dept_Name = "";
+            dept_Name = txtDep_Name.Text;
+            DataBase dataBase = new DataBase();
+            dataBase.InsertDepartment(dept_Name);
+            DataTable dtSalesmanResult = dataBase.GetDepartment();
+            gvDeptDetails.DataSource = dtSalesmanResult;
+            gvDeptDetails.DataBind();
+        }
+
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+        
+            DataBase dataBase = new DataBase();
+            string dept_num = ddldept_num.SelectedValue;
+            dataBase.UpdateDepartment(Convert.ToInt32(lblDep_num.Text),txtDep_Name.Text);
+            DataTable dtSalesmanResult = dataBase.GetDepartment();
+            gvDeptDetails.DataSource = dtSalesmanResult;
+            gvDeptDetails.DataBind();
+            lblstatus.Text = "Credentials Updated.Please check the table below";
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            txtDep_Name.Text = string.Empty;
+            lblDep_num.Text = string.Empty;
+        }
+
+        protected void gvDeptDetails_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int Dept_num = Convert.ToInt32(e.CommandArgument);
+            if (e.CommandName == "Edit")
+            {
+                string dept_num = ddldept_num.SelectedValue;
+                DataBase db = new DataBase();
+                DataTable dt = db.GetDepartmentbynum(Dept_num);
+                lblDep_num.Text = dt.Rows[0][0].ToString();
+                txtDep_Name.Text = dt.Rows[0][1].ToString();
+                lblstatus.Text = "Credentials Edited.Please check the table below";
+            }
+            else if (e.CommandName == "Delete")
+            {
+                DataBase db = new DataBase();
+                db.DeleteDept(Dept_num);
+                DataTable Result = db.GetDepartment();
+                gvDeptDetails.DataSource = Result;
+                gvDeptDetails.DataBind();
+                lblstatus.Text = "Credentials Deleted.Please check the table below";
+
+            }
+
+        }
+
+        protected void gvDeptDetails_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+        }
+
+        protected void gvDeptDetails_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ Assignment4-08-10-21/Assignment4.sql
@@ -0,0 +1,43 @@
+Create database EmployeeManagement;
+
+use EmployeeManagement;
+
+Create table Project (project_number numeric(5) primary key identity,
+						proj_name varchar(50) not null,
+						startdate date not null  
+						);
+
+Create table Department (dept_number numeric(5) primary key identity(100,1),
+						dept_name varchar(50) not null
+						);
+
+Create table Employee (emp_id numeric(10) primary key identity(1000,1),
+						title varchar(20) not null,
+						first_name varchar(20) not null,
+						last_name varchar(20) not null,
+						gender varchar(10) not null,
+						DOB date not null,
+						Hired_date date not null,
+						dept_number numeric(5) references Department(dept_number) on delete cascade ,
+						project_number numeric(5) references Project(project_number) on delete cascade
+						);
+drop table Employee
+drop table Department
+
+insert into Project values('proj1','2012-10-10');
+insert into Project values('Data science','2021-07-26');
+insert into Project values('Digit recognition','2020-09-17');
+insert into Project values('DSA','2021-05-19');
+insert into Project values('Face recognition','2020-01-10');
+insert into Project values('Machine learning','2021-06-26');
+
+insert into Department values('IT');
+insert into Department values('HR');
+insert into Department values('Security');
+insert into Department values('Marketing');
+insert into Department values('Administration');
+
+insert into Employee values('ASE', 'Rakesh', 'Thakur','Male','1999-06-27', '2021-09-02', 100, 3);
+insert into Employee values('ASE', 'Nagendra', 'Sai','Male','1999-08-10', '2021-09-02', 100, 2);
+insert into Employee values('ASE', 'Sanket', 'V','Male','1999-10-15', '2021-09-02', 100, 6);
+insert into Employee values('ASE', 'Sneha', 'Lalita','Female','1999-05-06', '2021-09-02', 100, 5)
